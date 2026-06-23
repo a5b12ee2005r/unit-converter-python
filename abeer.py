9af21109afd8dcd57749215a6ad8074bcca4c947
Abeer Riaz <abeer@example.com>
2026-06-23 02:22:03 -0700

    first commit

---
+++ abeer.py
@@ -0,0 +1,68 @@
+
+##
+
+
+# print("hello world")
+# #string
+# name="abeer"
+
+# #integer
+# batch=3
+
+# #float
+# course=3.0
+
+# #boolean
+# istrue=True
+
+# print(name)
+# print(batch)
+# print(course)
+
+# hello=input("Your Name:")
+# print(hello)
+##calculatir practice
+
+
+# num1=20
+# num2=20
+# num1=float(input("Enter first name"))
+# num2=float(input("Enter second name"))
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+##calculator task
+
+
+
+
+num1=float(input("Enter first name :"))
+operator=input("enter any sign(+, -, *, /) :")
+num2=float(input("Enter first name :"))
+if operator=="+":
+    print("Result", num1+num2)
+elif operator=="-":
+    print("Result", num1-num2)
+elif operator=="*":
+    print("Result", num1*num2)
+elif operator=="/":
+    if num2 !=0:
+        print("Result", num1/num2)
+    else:
+        print("numer is not disvalid")
+else:
+    print("invalid character")
+
+
+
+
+
+
+
+
+
+
+
+
