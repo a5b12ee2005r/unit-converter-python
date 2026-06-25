fab62642bb7c5ad93f677918161e207474265a69
Abeer Riaz <abeer@example.com>
2026-06-24 21:53:19 -0700

    first commit

---
+++ cf,.py
@@ -0,0 +1,11 @@
+
+f =float(input("enter temperature:"))
+celcius =(f-32) *  5/9
+print("Fahrenheit :", {f})
+c = float(input("enter temperature:"))
+Fahrenheit = (c *9/5) + 532
+print("Celcius:", {c})
+if celcious=="c":
+    print("Result", Fahrenheit)
+elif Fahrenheit=="f":
+    print("Result",celcious )
